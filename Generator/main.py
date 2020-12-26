bb3727e1f7a7f44ecfdc4e87aed27c69448c6656
thom974 <74675902+thom974@users.noreply.github.com>
2020-12-26 15:20:27 -0500

    developing

--- Generator/main.py
+++ Generator/main.py
@@ -1,21 +1,163 @@
+import requests
+import json
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.ui import WebDriverWait
 import time
 
-driver = webdriver.Chrome('C:\Program Files (x86)\chromedriver.exe')
-driver.get('https://bloodcat.com/osu/')
 
-generated_maps = []  # store all maps, format type unknown for now
-desired_map_count = 15  # e.g after 15 maps, driver will stop scraping page
-t_end = time.time() + 3
+starting_time = time.time()
 
-# while time.time() < t_end:
-#     driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
-#     page_sc = driver.page_source
-#     print(page_sc)
 
-map_str = driver.find_elements_by_tag_name("a")
-for beatmap in map_str:
-    print("line:", beatmap.text)
+def return_sc(driver):
+    return driver.page_source
 
-driver.quit()
+
+def fetch_maps(num):
+    generated_maps = []  # store all maps, format type unknown for now
+    xpath = "//main[@class]//a[@class='id']"
+    desired_map_count = num
+
+    driver = webdriver.Chrome('C:\Program Files (x86)\chromedriver.exe')
+    driver.get('https://bloodcat.com/osu/?q=&c=b&m=0&s=&g=&l=')
+
+    while len(generated_maps) < desired_map_count:
+        WebDriverWait(driver, timeout=5).until(return_sc)
+        map_str = driver.find_elements_by_xpath(xpath)
+
+        for beatmap in map_str:
+            map_id = beatmap.get_attribute('text')
+
+            if map_id not in generated_maps:
+                generated_maps.append(map_id)
+
+            if len(generated_maps) >= desired_map_count:
+                desired_map_count = len(generated_maps)
+                break
+
+        driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
+    driver.quit()
+    return generated_maps
+
+
+def fetch_access_token():
+    with open('access_token.txt','r') as f:
+        file_contents = [line for line in f]
+
+    if len(file_contents) == 0:  # if there is no access token
+        # parameters used to construct initial authorization url for the client
+        req_user_perm = {'client_id':'1659',
+                         'redirect_uri': 'https://osu.ppy.sh/beatmapsets',
+                         'response_type': 'code',
+                         'scope': 'public'
+                         }
+        # from the response object, grab url
+        constructed_url = requests.get('https://osu.ppy.sh/oauth/authorize', params=req_user_perm).url
+
+        # send user to auth. page // this was using selenium
+        # driver = webdriver.Chrome('C:\Program Files (x86)\chromedriver.exe')
+        # driver.get(constructed_url)
+        # print(constructed_url)
+
+        print("Please visit this link to authorize the client:", constructed_url)
+        # fetch the auth_token from redirected URL
+        code_for_auth = input("Copy and paste the URL of the redirected site after authorizing:").split("=")[1]
+
+        # fetch authorization token
+        auth_body_data = {'client_id':'1659',
+                          'client_secret': 'kQYctkFBxE2vVWNlSzZdmPlLtBYdFE8a2m3cPrlE',
+                          'code': code_for_auth,
+                          'grant_type': 'authorization_code',
+                          'redirect_uri': 'https://osu.ppy.sh/beatmapsets'
+                          }
+        code_response_text = requests.post('https://osu.ppy.sh/oauth/token', data=auth_body_data).text
+        access_token = code_response_text.split(",")[2].split(":")[1][1:-1]
+
+        with open('access_token.txt','w') as f:
+            f.write(access_token)
+
+
+# how the maps are filtered
+def map_filter(dct,map_filters):
+    beatmapset_diffs = dct
+    print(beatmapset_diffs)
+
+    # find top diff
+    diff_star_ratings = [dct['difficulty_rating'] for dct in beatmapset_diffs]  # holds the star rating of each diff
+    diff_star_ratings.sort()
+    for diff in beatmapset_diffs:
+        if diff['difficulty_rating'] == diff_star_ratings[-1]:
+            top_diff = diff
+            break
+
+    # check star rating
+    if not diff_star_ratings[-1] > map_filters['stars']:
+        return False
+
+    # check map set total play count
+    diff_play_counts = sum([dct['playcount'] for dct in beatmapset_diffs])
+    if not diff_play_counts >= 100:
+        return False
+
+    # check length of map
+    diff_length = top_diff['total_length']
+    if not diff_length >= map_filters['len']:
+        return False
+
+    # check ar of map
+    diff_ar = top_diff['ar']
+    if not diff_ar >= map_filters['ar']:
+        return False
+
+    # check bpm of map
+    diff_bpm = top_diff['bpm']
+    if not diff_bpm >= map_filters['bpm']:
+        return False
+
+    # check cs of map
+    diff_cs = top_diff['cs']
+    if not diff_cs <= map_filters['cs']:
+        return False
+
+    # check status
+    diff_status = top_diff['status']
+    if not diff_status == map_filters['status'] and map_filters['status'] != 'any':
+        return False
+
+    return True
+
+
+def fetch_new_maps(lst,map_filters):
+    maps_to_filter = lst
+    new_maps = []
+
+    with open('access_token.txt','r') as f:
+        access_token = f.readline().rstrip("\n")
+    token_header = {'Authorization': 'Bearer ' + access_token}
+
+    for map_id in maps_to_filter:
+        beatmapset_info = requests.get("https://osu.ppy.sh/api/v2/beatmapsets/" + str(map_id), headers=token_header).json()
+        beatmapset_diffs = beatmapset_info['beatmaps']  # holds all difficulties of the map set
+
+        diff_star_ratings = [dct['difficulty_rating'] for dct in beatmapset_diffs]  # holds the star rating of each diff
+        diff_star_ratings.sort()
+
+        if map_filter(beatmapset_diffs,map_filters):
+            new_maps.append(beatmapset_diffs[-1]['beatmapset_id'])
+
+    return new_maps
+
+fetch_access_token()
+users_maps = fetch_maps(1000)
+filters = {'len':60,
+           'stars':7,
+           'ar':9,
+           'bpm':160,
+           'cs':4.2,
+           'status': 'any'
+           }
+maps = fetch_new_maps(users_maps,filters)
+print(maps)
+print(time.time() - starting_time, "seconds")
+
